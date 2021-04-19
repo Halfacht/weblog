b215e9f690234772767b4df7d7c84fc04f24ea88
Halfacht <alwinytsma@gmail.com>
2021-04-19 13:00:35 +0200

    Fix register

--- resources/js/views/Register.vue
+++ resources/js/views/Register.vue
@@ -1,46 +1,46 @@
 <template>
-  <div class="container">
-    <success-message :message="form.successMessage"></success-message>
+    <div class="container">
+        <success-message :message="form.successMessage"></success-message>
 
-    <form class="row g-3" @keydown="form.errors.clear($event.target.name)">
-      <div class="col">
-        <input-field
-          name="name"
-          v-model="form.name"
-          :error="form.errors.get('name')"
-        ></input-field>
+        <form class="row g-3" @keydown="form.errors.clear($event.target.name)">
+            <div class="col">
+                <input-field
+                    v-model="form.name"
+                    :error="form.errors.get('name')"
+                    name="name"
+                ></input-field>
 
-        <input-field
-          name="email"
-          type="email"
-          v-model="form.email"
-          :error="form.errors.get('email')"
-        ></input-field>
+                <input-field
+                    v-model="form.email"
+                    :error="form.errors.get('email')"
+                    name="email"
+                    type="email"
+                ></input-field>
 
-        <input-field
-          name="password"
-          type="password"
-          v-model="form.password"
-          :error="form.errors.get('password')"
-        ></input-field>
+                <input-field
+                    v-model="form.password"
+                    :error="form.errors.get('password')"
+                    name="password"
+                    type="password"
+                ></input-field>
 
-        <input-field
-          name="password_confirmation"
-          type="password"
-          v-model="form.password_confirmation"
-          :error="form.errors.get('password_confirmation')"
-        ></input-field>
+                <input-field
+                    v-model="form.password_confirmation"
+                    :error="form.errors.get('password_confirmation')"
+                    name="password_confirmation"
+                    type="password"
+                ></input-field>
 
-        <button
-          class="btn btn-success"
-          @click.prevent="register"
-          :disabled="formDisabled"
-        >
-          Register
-        </button>
-      </div>
-    </form>
-  </div>
+                <button
+                    :disabled="formDisabled"
+                    class="btn btn-success"
+                    @click.prevent="register"
+                >
+                    Register
+                </button>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
@@ -48,29 +48,29 @@ import InputField from "@/components/form-parts/InputField.vue";
 import SuccessMessage from "@/components/parts/SuccessMessage.vue";
 
 export default {
-  components: { InputField, SuccessMessage },
+    components: {InputField, SuccessMessage},
 
-  data() {
-    return {
-      form: new Form({
-        name: "",
-        email: "",
-        password: "",
-        password_confirmation: "",
-      }),
-    };
-  },
-
-  computed: {
-    formDisabled() {
-      return this.form.errors.hasErrors() || this.form.isSubmitting;
+    data() {
+        return {
+            form: new Form({
+                name: "",
+                email: "",
+                password: "",
+                password_confirmation: "",
+            }),
+        };
     },
-  },
 
-  methods: {
-    register() {
-      this.form.post("/api/register");
+    computed: {
+        formDisabled() {
+            return this.form.errors.hasErrors() || this.form.isSubmitting;
+        },
     },
-  },
+
+    methods: {
+        register() {
+            this.form.action(this.$store, "/api/register");
+        },
+    },
 };
 </script>
